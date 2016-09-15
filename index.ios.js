e49c18c345be4919c564b5a2c9623d75e43394a8
Wellington Dessuy <wellington.dessuy@outlook.com>
2016-09-15 16:30:10 -0300

    index.ios

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { AppRegistry, Navigator } from 'react-native';
+import MenuScreen from './app/src/screens/MenuScreen';
+import ServicesScreen from './app/src/screens/ServicesScreen';
+import ServiceDataScreen from './app/src/screens/ServiceDataScreen';
+import PaymentScreen from './app/src/screens/PaymentScreen';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+class App extends React.Component {
 
-class NewTest extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+	render() {
+		return (
+			<Navigator
+				configureScene={ () => this.configureScene() }
+				style={{ flex:1}}
+				initialRoute={{id: 'menu'}}
+				renderScene={this.renderScene}
+				/>
+			);
+	}
+
+	configureScene(route, routeStack) {
+    return Navigator.SceneConfigs.PushFromRight;
   }
+
+	renderScene(route, navigator) {
+		_navigator = navigator;
+		switch (route.id) {
+			case 'menu':
+				return (<MenuScreen navigator={_navigator} title="menu"/>);
+			case 'services':
+				return (<ServicesScreen navigator={_navigator} title="services" passProps={route.passProps}/>);
+			case 'service_data':
+				return (<ServiceDataScreen navigator={_navigator} title="service_data" passProps={route.passProps}/>);
+			case 'payment':
+				return (<PaymentScreen navigator={_navigator} title="payment" passProps={route.passProps}/>);
+		  default:
+			  return (<MenuScreen navigator={_navigator} title="menu"/>);
+		}
+	}
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('NewTest', () => NewTest);
+AppRegistry.registerComponent('NewTest', () => App);
